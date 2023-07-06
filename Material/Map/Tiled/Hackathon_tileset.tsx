73638bbbd6e00eba7aecc8e44ab85149136e2413
Haizues1210 <104113556+Haizues1210@users.noreply.github.com>
2023-07-06 18:45:14 +0700

    Map 1_1 + tileset
    Done

---
+++ Material/Map/Tiled/Hackathon_tileset.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset" tilewidth="32" tileheight="32" tilecount="160" columns="16">
+ <image source="../../../../../Game dev/Map material/tileset.png" width="512" height="337"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="3" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="4" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="16" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="17" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="19" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="20" wangid="0,1,1,1,1,1,0,0"/>
+   <wangtile tileid="32" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="33" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="34" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
